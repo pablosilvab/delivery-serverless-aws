a2fe1e31e7555b558082dbd646a34b72637e2e8f
Pablo Silva <pablonicolassilvabravo@gmail.com>
2020-05-13 18:25:04 -0400

    fix uuid

--- handler.js
+++ handler.js
@@ -1,15 +1,15 @@
 'use strict';
 
-const uuid = require('uuid/v1');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports.makeOrder = async event => {
-  const orderID = uuid();
+  const orderId = uuidv4(); 
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'El pedido fue registrado con el numero de orden ${orderID}'
+        message: `El pedido fue registrado con el numero de orden: ${orderId}`
       },
       null,
       2
